7b4cf03fd05524e804fda1f089998e319669933d
xxxx <sam@corcos.io>
2019-01-18 22:44:21 -0500

    update navbar styling

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import Link from 'next/link'
 
 class Navbar extends React.Component {
   render () {
     return (
-      <nav>
-        foobar
+      <nav className='navbar'>
+        <Link href='/'>
+          <div>
+            Logo
+          </div>
+        </Link>
+        <div className='navbar-action-container'>
+          Log in?
+        </div>
+
+        <style jsx>{`
+         .navbar {
+            height: 60px;
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            border-bottom: 1px solid black;
+          }  
+          .navbar-action-container {
+            margin-right: 15px;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+          }  
+        `}</style>
       </nav>
     )
   }
